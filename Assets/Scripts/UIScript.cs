735acd1de7c02cc688aed215c108d6d34bbd357d
Dominic Zahn <zahndominic@web.de>
2020-03-22 14:03:09 +0100

    basic UI
    -FPS counter
    -pause Menu shows up when pressed escape
    -resume button functionality
    -exit button functionality
    TODO:
    - options and menu buttton
    - pause the game when entered pause menu

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -7,21 +7,49 @@ using System.Linq;
 public class UIScript : MonoBehaviour
 {
     public Text fpsText;
+    public GameObject pauseMenu;
+    private const KeyCode menuKey = KeyCode.Escape;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
         updateFps();
+        if (Input.GetKeyDown(menuKey))
+            togglePanel();
     }
 
     void updateFps()
     {
         fpsText.text = (int)(1.0 / Time.deltaTime) + " FPS";
+    }
+
+    void togglePanel()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
+    public void resume()
+    {
+        togglePanel();
+    }
+
+    public void options()
+    {
+        // TODO
+    }
+
+    public void menu()
+    {
+        // TODO
+    }
+
+    public void exit()
+    {
+        Application.Quit();
     }
 }
