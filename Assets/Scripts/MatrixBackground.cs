f5172cfcb906610c33334eb64acef79a4fbf6e9d
Dominic Zahn <zahndominic@web.de>
2020-03-25 22:05:23 +0100

    tested color change
    created little example

--- Assets/Scripts/MatrixBackground.cs
+++ Assets/Scripts/MatrixBackground.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MatrixBackground : MonoBehaviour
 {
-    
+    public TextMeshProUGUI background;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        background.richText = true;
+        string oldText = background.text;
+        background.text = $"Coloured text <color=red>{ oldText }</color>";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
